03b5f7b3fe7c248fb092bba44ae5a1c19fdec9c8
Tomer Ben Shimol <tomerbenshimol3164@gmail.com>
2023-02-16 14:27:04 +0200

    JS implementation

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-modal");
+const btnsOpenModel = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closedModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++) {
+  btnsOpenModel[i].addEventListener("click", openModal);
+}
+
+btnCloseModel.addEventListener("click", closedModal);
+overlay.addEventListener("click", closedModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.code === "Escape" && !modal.classList.contains("hidden")) {
+    closedModal();
+  }
+});
